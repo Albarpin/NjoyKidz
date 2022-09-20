784badba38af98ba330e54cf9104b92f0b781cd1
Ali Baran Al <alibaran_al@hotmail.com>
2022-09-21 00:14:20 +0300

    update

--- Assets/Scriptss/GridManager.cs
+++ Assets/Scriptss/GridManager.cs
@@ -37,7 +37,7 @@ public class GridManager : MonoBehaviour
 
         float gridW = cols * tileSize;
         float gridH = rows * tileSize;
-        transform.position = new Vector2(-gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);
+        transform.position = new Vector2(-gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);   //Ekran boyutu ne olursa olsun düzgün gözükmesi için
        
 
        
